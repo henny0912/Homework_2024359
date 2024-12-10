14d6821d4b476f2cc76a9ada987cb10c9287f572
henny0912 <2024359@student.cct.ie>
2024-12-10 14:43:42 +0000

    Empty project.

---
+++ src/stringmethodhomework/StringMethodHomework.java
@@ -0,0 +1,22 @@
+/* Homework for String Methods
+ * Student Number: 2024359
+ * Name: Egshiglen Enkhbayar
+ */
+
+package stringmethodhomework;
+
+/**
+ *
+ * @author egshi
+ */
+public class StringMethodHomework {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
